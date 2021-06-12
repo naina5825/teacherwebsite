20138e6cb0675fedad3c07472f2d9e1cdd14c129
Naina <naina9814saini@gmail.com>
2021-06-12 18:02:27 +0530

    final website

--- js/script.js
+++ js/script.js
@@ -8,4 +8,15 @@ $(function(){
 })
 
 
+$(function(){
+        function loop(){
+            $(".about-part h1")
+            .animate({fontSize:0},1000)
+            .animate({fontSize:45},3000)
 
+            .animate({marginLeft:80},1000)
+            .animate({marginLeft:0},2000,loop)
+        }
+        loop();
+})
+ 
